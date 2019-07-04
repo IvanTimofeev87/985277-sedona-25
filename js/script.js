2736bc9b1a5c79ea786431549d0efddbf5d598ab
IvanTimofeev87 <49988354+IvanTimofeev87@users.noreply.github.com>
2019-07-04 17:36:14 +0300

    теперь точно все

--- js/script.js
+++ js/script.js
@@ -62,14 +62,34 @@ window.addEventListener("keydown", function (evt) {
 
 var link = document.querySelector(".search-button");
 var popup = document.querySelector(".hotels-search-date-form");
+var check_in = popup.querySelector(".check-in-date");
+var check_out = popup.querySelector(".check-out-date");
+var adult = popup.querySelector(".adult-value");
+var children = popup.querySelector(".children-value");
 
 link.addEventListener("click", function (evt) {
   evt.preventDefault();
   if (popup.classList.contains("modal-open") && popup.classList.contains("modal-show")) {
     popup.classList.add("visually-hidden");
     popup.classList.remove("modal-show");
+    popup.classList.remove("modal-error");
+  }
+  else if (popup.classList.contains("first-click")) {
+    popup.classList.add("visually-hidden")
+    popup.classList.remove("modal-show");
+    popup.classList.remove("modal-error");
+    popup.classList.remove("first-click");
   }
   else {
     popup.classList.add("modal-show");
   }
 })
+
+popup.addEventListener("submit", function (evt) {
+  if (!check_in.value || !check_out.value || !adult.value || !children.value) {
+    evt.preventDefault();
+    popup.classList.remove("modal-error");
+    popup.offsetWidth = popup.offsetWidth;
+    popup.classList.add("modal-error");
+  }
+});
